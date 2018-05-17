e824f33900e4057c8be5560cb3ec2442400ebd00
Michael Ritter <ritter.michael92@gmail.com>
2018-05-17 18:35:36 +0200

    Add invite functionality to /api/discord enpoint
    The `/api/discord` endpoint should now additionally add the user
    to a configured Discord guild, if configured.
    
    Add the two environment variables `BOT_TOKEN` and `GUILD_ID` for this.
    
    This requires some tweaks to how api requests are sent/handled:
    - It should be possible to override all request fields (for headers)
    - The response code should be considered for error checks and
      exposed via promise return (201 vs 204)

--- containers/node/routes/discord.js
+++ containers/node/routes/discord.js
@@ -10,6 +10,10 @@ const discordApiBase = 'https://discordapp.com/api/v6/'
 
 const clientId = process.env.CLIENT_ID || '';
 const clientSecret = process.env.CLIENT_SECRET || '';
+
+const botToken = process.env.BOT_TOKEN || '';
+const guildId = process.env.GUILD_ID || '';
+
 const redirectUri = 'https://' + (process.env.HOST || 'localhost') + '/api/discord/callback';
 
 //only continue if id and secret are set
@@ -21,20 +25,25 @@ if(clientId !== '' || clientSecret !== '') {
         redirectUri,
         accessTokenUri: 'https://discordapp.com/api/oauth2/token',
         authorizationUri: 'https://discordapp.com/api/oauth2/authorize',
-        scopes: ['identify', 'connections']
+        scopes: ['identify', 'connections', 'guilds.join']
     });
     
-    function fetch(apiPath, token) {
-        return new Promise((resolve, reject) => {
-            request({
+    function fetch(apiPath, token, fieldOverrides = {}) {
+        const fields = Object.assign({}, {
                 url: discordApiBase + apiPath,
                 headers: {'Authorization': 'Bearer ' + token},
                 json: true
-            }, (error, response, body) => {
+            }, fieldOverrides);
+        return new Promise((resolve, reject) => {
+            request(fields, (error, response, body) => {
                 if(error)
-                    reject(error);
+                    return reject(error);
+
+                const code = response.statusCode;
+                if(code < 200 || code > 299)
+                    reject({code, body});
                 else
-                    resolve(body);
+                    resolve({code, body});
             });
         });
     }
@@ -50,21 +59,49 @@ if(clientId !== '' || clientSecret !== '') {
     router.get('/callback', (req, res) => {
         discordAuth.code.getToken({query: req.query}) //hide pathname due to being reverse proxied
         .then((tokens) => {
+            const token = tokens.accessToken;
+
+            let responseContent = '';
+
             Promise.all([
-                fetch('users/@me', tokens.accessToken),
-                fetch('users/@me/connections', tokens.accessToken)
-            ]).then(([user, connections]) => {
+                fetch('users/@me', token),
+                fetch('users/@me/connections', token)
+            ])
+            //handle user info + connections, add to guild
+            .then(([{body: user}, {body: connections}]) => {
                 let response = `Connections for ${user.username}#${user.discriminator} (${user.id})<br/>`;
                 let connectionString = connections
                     //.filter(conn => conn.verified)
                     .map(conn => `<li>${normalizeString(conn.type)}: ${conn.name}</li>`)
                     .join('');
                 connectionString = (connectionString === '') ? 'No Connections' : '<ul>'+connectionString+'</ul>';
-                res.status(200).send(response + connectionString);
-            }).catch(err => {
+                responseContent = response + connectionString;
+                if(botToken !== '' && guildId !== '')
+                    return fetch(`guilds/${guildId}/members/${user.id}`, false, {
+                        method: 'PUT',
+                        headers: {'Authorization': 'Bot ' + botToken},
+                        body: { 'access_token': token }
+                    });
+                else
+                    return false;
+            })
+            //handle response of guild add and write out everything
+            .then((addResponse) => {
+                if(addResponse === false) //handle addition not being enabled
+                    return res.send(responseContent);
+
+                if(addResponse.code === 204)
+                    res.send(responseContent + "<br/>And you are already in the JDA Guild :)");
+                else
+                    res.send(responseContent + "<br/>Oh btw: You are now a proud member of the JDA Fanclub (JDA Guild)");
+            })
+            .catch(err => {
                 console.error(err);
-                res.status(500).send("There was an error fetching your data!");
-            });
+                if(responseContent !== '')
+                    res.send(responseContent);
+                else
+                    res.status(500).end("There was an error fetching your data!");
+            })
         });
     });
 } else {
